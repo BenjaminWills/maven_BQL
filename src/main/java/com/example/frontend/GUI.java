6949a4abd7716b6624f0efcdbb81bfd67fdf1e1c
Benjaminwills <benjaminwills047@gmail.com>
2023-01-29 21:33:24 +0000

    got my first gui!!

--- src/main/java/com/example/frontend/GUI.java
+++ src/main/java/com/example/frontend/GUI.java
@@ -1,5 +1,29 @@
 package com.example.frontend;
 
+import java.awt.*;
+import javax.swing.*;
+
 public class GUI {
-    
+
+    public GUI(){
+
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(
+            BorderFactory.createEmptyBorder(30, 30, 10, 30)
+        );
+        panel.setLayout(
+            new GridLayout(0,1)
+        );
+
+        frame.add(panel,BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("BQL");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new GUI();
+    }
 }
