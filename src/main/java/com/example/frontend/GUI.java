59ff46af4e6ad116937756923c5e77a8589e239e
Benjaminwills <benjaminwills047@gmail.com>
2023-01-29 21:37:49 +0000

    add button and label

--- src/main/java/com/example/frontend/GUI.java
+++ src/main/java/com/example/frontend/GUI.java
@@ -9,6 +9,9 @@ public class GUI {
 
         JFrame frame = new JFrame();
 
+        JButton button = new JButton("Add databases");
+        JLabel label = new JLabel("Number of Databases: 0");
+
         JPanel panel = new JPanel();
         panel.setBorder(
             BorderFactory.createEmptyBorder(30, 30, 10, 30)
@@ -16,6 +19,8 @@ public class GUI {
         panel.setLayout(
             new GridLayout(0,1)
         );
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel,BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
