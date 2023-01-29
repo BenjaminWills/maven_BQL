f5e867710d3b362ffa786edd09985731fc9c8297
Benjaminwills <benjaminwills047@gmail.com>
2023-01-29 22:01:16 +0000

    sort imports

--- src/main/java/com/example/frontend/GUI.java
+++ src/main/java/com/example/frontend/GUI.java
@@ -1,8 +1,15 @@
 package com.example.frontend;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GUI implements ActionListener{
 
