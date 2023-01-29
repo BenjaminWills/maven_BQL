d34ae0c5c534e986402947b6fc039ed8222a7ffd
Benjaminwills <benjaminwills047@gmail.com>
2023-01-29 22:40:49 +0000

    yeet im off to bed

--- src/main/java/com/example/frontend/APIRequests.java
+++ src/main/java/com/example/frontend/APIRequests.java
@@ -1,5 +1,9 @@
 package com.example.frontend;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class APIRequests {
+    URL url = new URL("http://127.0.0.1:4567/database/testDatabaseFour.bql");
     
 }
