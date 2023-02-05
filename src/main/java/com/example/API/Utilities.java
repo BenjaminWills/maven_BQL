53810eb286c704d8f0113a83aa8a918c7e62bc9c
Benjaminwills <benjaminwills047@gmail.com>
2023-02-05 20:13:03 +0000

    add list database name function

--- src/main/java/com/example/API/Utilities.java
+++ src/main/java/com/example/API/Utilities.java
@@ -1,12 +1,36 @@
 package com.example.API;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.json.JSONObject;
 
+import com.example.backend.Database.Database;
+import com.example.backend.storage.BQLStorageBlock;
+
 public class Utilities {
+
     public static JSONObject HashmapToJSON(HashMap<String,Object> rowContents){
-        JSONObject json = new JSONObject(rowContents);
+        JSONObject json;
+        json = new JSONObject(rowContents);
         return json;
+    }
+
+    public static List<Database> getDatabases(BQLStorageBlock storageBlock){
+        List<Database> databases;
+        databases = storageBlock.databases;
+        return databases;
+    }
+
+    public static ArrayList<String> listDatabaseNames(BQLStorageBlock storageBlock){
+        ArrayList<String> databaseNameList;
+        
+        databaseNameList = new ArrayList<String>();
+
+        for (Database db : getDatabases(storageBlock)){
+            databaseNameList.add(db.databaseName);
+        }
+        return databaseNameList;
     }
 }
