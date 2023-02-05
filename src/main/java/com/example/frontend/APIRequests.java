fa3d00d90c38fdb9470d0e165fc15f656e06e530
Benjaminwills <benjaminwills047@gmail.com>
2023-02-05 19:33:35 +0000

    working http request to api :D

--- src/main/java/com/example/frontend/APIRequests.java
+++ src/main/java/com/example/frontend/APIRequests.java
@@ -1,9 +1,43 @@
 package com.example.frontend;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class APIRequests {
-    // URL url = new URL("http://127.0.0.1:4567/database/testDatabaseFour.bql");
+    public static void main(String[] args) throws IOException{
+
+        URL url = new URL("http://127.0.0.1:4567/database/testDatabaseFour.bql");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type","application/json");
+        connection.setRequestProperty("Accept","application/json");
+
+        HttpURLConnection.setFollowRedirects(true);
+
+        connection.setInstanceFollowRedirects(false);
+
+        InputStream ip = connection.getInputStream();
+ 
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(ip));
+
+        StringBuilder response
+                    = new StringBuilder();
+                String responseSingle = null;
+ 
+                while ((responseSingle = br1.readLine())
+                       != null) {
+                    response.append(responseSingle);
+                }
+                String xx = response.toString();
+                System.out.println(xx);
+            }
+
+    }
     
-}
+
