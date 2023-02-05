85d93d487ce3497ca92423f356a6bd26b2635d74
Benjaminwills <benjaminwills047@gmail.com>
2023-02-05 19:38:22 +0000

    make get request function

--- src/main/java/com/example/frontend/APIRequests.java
+++ src/main/java/com/example/frontend/APIRequests.java
@@ -8,6 +8,32 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class APIRequests {
+
+    public static void getRequest(String requestURL) throws IOException{
+        URL url = new URL(requestURL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type","application/json");
+        connection.setRequestProperty("Accept","application/json");
+
+        HttpURLConnection.setFollowRedirects(true);
+
+        connection.setInstanceFollowRedirects(false);
+
+        InputStream ip = connection.getInputStream();
+ 
+        BufferedReader streamReader = new BufferedReader(new InputStreamReader(ip));
+
+        StringBuilder response = new StringBuilder();
+        String responseSingle = null;
+ 
+        while ((responseSingle = streamReader.readLine()) != null) {
+            response.append(responseSingle);
+        }
+        String xx = response.toString();
+        System.out.println(xx);
+    }
     public static void main(String[] args) throws IOException{
 
         URL url = new URL("http://127.0.0.1:4567/database/testDatabaseFour.bql");
@@ -38,6 +64,6 @@ public class APIRequests {
                 System.out.println(xx);
             }
 
-    }
+}
     
 
